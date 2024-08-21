26f92a8851a2655fae8a7472674f7cd62b06f190
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-08-22 01:04:26 +0600

    code sending

---
+++ Count.js
@@ -0,0 +1,43 @@
+const Num = document.getElementById("num")
+
+
+
+let n = parseInt(localStorage.getItem("n"))
+Num.innerText = n;
+
+
+
+function increment() {
+     n++
+     Num.innerText = n;
+     localStorage.setItem("n", n)
+
+
+}
+function Descrement() {
+     if (n < 0) {
+          n--
+          Num.innerText = n;
+          localStorage.setItem("n", n)
+     }
+
+
+}
+function reset() {
+     n = 0;
+     Num.innerText = n;
+     localStorage.setItem("n",n)
+
+}
+function add(event) {
+     event.preventDefault()
+     const inputValue = parseInt(document.getElementById('value').value)
+     console.log(inputValue);
+     if (!isNaN(inputValue)) {
+          n += inputValue;
+          Num.innerText = n;
+          localStorage.setItem("n", n)
+     }
+
+}
+
